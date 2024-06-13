b29c1b7ad4ee91c70b63ea6e2786f6d7a8dd3bfd
WilliamAmorosi <william.amorosi1234@gmail.com>
2024-06-13 15:47:15 -0400

    part of the README done

--- Station_Plot.R
+++ Station_Plot.R
@@ -4,14 +4,20 @@ library("tidyverse")
 
 #city centroid
 stations <- read.csv("Coordinates.cvs")
+sources <- read.csv("Poll_Coordinates.cvs")
 
 df<-stations |>
   select(lon2, lat2)
 
+df2<-sources |>
+  select(lon2, lat2)
+
 #converts df into a spatvector
 x <- vect(df, geom=c("lon2", "lat2"), crs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ")
+y <- vect(df2, geom=c("lon2", "lat2"), crs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ")
 
 plot(x)
+plot(y)
 
 #check unit of x
 crs(x)
@@ -27,5 +33,11 @@ extent<-buffer(x, width = 400)
 bg <- get_tiles(ext(extent))
 
 plot(bg)
-points(x)
+
+plot(x)
+#points(x)
 lines(pts_buffer)
+points(y, col = "red")
+
+outfile <- "buffer.shp"
+writeVector(pts_buffer, outfile, overwrite=TRUE)
